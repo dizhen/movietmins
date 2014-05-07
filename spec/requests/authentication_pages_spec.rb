d42172ae586c58b03431498326b9cde8f30b21c7
Zhen <di3@wisc.edu>
2014-05-06 23:05:26 -0500

    partly finish user update users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,6 +11,29 @@ describe "Authentication" do
     it { should have_title('Sign in') }
   end
 
+
+describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
+  end
+
+
+
 describe "signin" do
     before { visit signin_path }
 
@@ -26,6 +49,9 @@ describe "signin" do
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
+
+
 
       describe "followed by signout" do
         before { click_link "Sign out" }
@@ -46,7 +72,5 @@ describe "signin" do
       end
     end
   end
-
-
 
 end
